aaaa8ff79d45f5a01237ab73d98923a2c20b7dd2
Dan Tennery-Spalding <teachrdan@gmail.com>
2018-09-03 15:45:25 -0700

    adds variable orbital distance

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+/* global d3 */
 window.d3 = d3
 // from "Placing n elements around a circle with radius r"
 // http://bl.ocks.org/bycoffe/3404776
 // magic numbers, all in pixels
-let distanceBetweenShells = 30
 const height = 750
 const width = 1000
 
@@ -22,8 +22,9 @@ const drawPursuance = function () {
     .attr('fill', 'red')
 }
 
-let createNodes = function (numNodes, shellNumber) {
-  const firstShellRadius = parseInt(document.getElementById('first-orbit-radius').value)
+let createNodes = function (numNodes, OrbitNumber) {
+  const firstOrbitRadius = parseInt(document.getElementById('first-orbit-radius').value)
+  const distanceBetweenOrbits = parseInt(document.getElementById('distance-between-orbits').value)
   let nodes = []
   let angle
   let x
@@ -32,9 +33,9 @@ let createNodes = function (numNodes, shellNumber) {
     // Calculate the angle at which the element will be placed.
     angle = (i / (numNodes / 2)) * Math.PI
     // Calculate the x position of the element.
-    x = ((firstShellRadius + (distanceBetweenShells * shellNumber)) * Math.cos(angle)) + (width / 2)
+    x = ((firstOrbitRadius + (distanceBetweenOrbits * OrbitNumber)) * Math.cos(angle)) + (width / 2)
      // Calculate the y position of the element.
-    y = ((firstShellRadius + (distanceBetweenShells * shellNumber)) * Math.sin(angle)) + (height / 2)
+    y = ((firstOrbitRadius + (distanceBetweenOrbits * OrbitNumber)) * Math.sin(angle)) + (height / 2)
     nodes.push({'id': i, 'x': x, 'y': y})
   }
   return nodes
@@ -52,24 +53,24 @@ let createElements = function (nodes) {
   })
 }
 
-let draw = function (numNodes, shellNumber) {
-  let nodes = createNodes(numNodes, shellNumber)
-  createElements(nodes, shellNumber)
+let draw = function (numNodes, OrbitNumber) {
+  let nodes = createNodes(numNodes, OrbitNumber)
+  createElements(nodes, OrbitNumber)
 }
 
 let drawOrbits = function () {
   d3.selectAll('.node').remove()
-  num = parseInt(document.getElementById('num-users').value)
+  let num = parseInt(document.getElementById('num-users').value)
   let capacities = [2, 8, 10, 14, 18]
-  let shellNumber = 0
+  let OrbitNumber = 0
 
-  while (num > 0 && shellNumber < 10) {
-    let thisShellCapacity = capacities[shellNumber] ? capacities[shellNumber] : 18 + shellNumber * 5
+  while (num > 0 && OrbitNumber < 10) {
+    let thisShellCapacity = capacities[OrbitNumber] ? capacities[OrbitNumber] : 18 + OrbitNumber * 5
     // in case there are more spaces in this orbit than people to fill it
     let nodesInShell = (thisShellCapacity > num) ? num : thisShellCapacity
-    draw(nodesInShell, shellNumber)
+    draw(nodesInShell, OrbitNumber)
     num -= thisShellCapacity
-    shellNumber++
+    OrbitNumber++
   }
 }
 
